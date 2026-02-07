672153f94281ed9162f5cf6298e9079fdb784733
anthonysanderslds <asanders2@buypathway.edu>
2026-02-07 01:58:31 -0600

    week 5 work

---
+++ scripts/form.js
@@ -0,0 +1,53 @@
+const today = new Date();
+const currentYearElement = document.getElementById("currentYear");
+currentYearElement.textContent = today.getFullYear();
+document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
+PopulateProductDropdown(products);
+
+function PopulateProductDropdown(productArray) {
+
+    const productDropdown = document.querySelector("#productName");
+
+    productArray.forEach(product => {
+        const option = document.createElement("option");
+        option.setAttribute("value", product.id);
+        option.textContent = toTitleCase(product.name);
+        productDropdown.appendChild(option);
+    });
+
+}
+
+function toTitleCase(str) {
+    return str.toLowerCase().split(' ').map(word => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+}
